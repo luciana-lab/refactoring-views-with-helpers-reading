1d6d534ce4819b1ec6d848a44fcf51a0638fe1e3
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-13 13:45:39 -0400

    commit tree 9d7cf5b360fa93c86f6d1e45c83e6f5294bb53de

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    #send text to the place in our application layout that is expecting some content for the :title
+    def title(text)
+        content_for :title, text
+    end
 end
